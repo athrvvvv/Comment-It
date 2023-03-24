1c79fc805b6087285af1c36f3a317e051a888adc
athrvvv <codingbruh.ac@gmail.com>
2023-03-25 02:10:14 +0530

    Its legal to add them

---
+++ Javascript/comment.js
@@ -0,0 +1,4 @@
+// Comment
+
+/* What are the strongest days of the week?
+Saturday and Sunday. All the rest are weak-days. */
---
+++ Typescript/comment.ts
@@ -0,0 +1,4 @@
+// Comment
+
+/* What do you get when you cross the Atlantic with the Titanic?
+About half-way. */
